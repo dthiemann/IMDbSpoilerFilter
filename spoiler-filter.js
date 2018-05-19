5c147fdfb9a66ea5a786f95e9bbca9779689915a
Dylan Thiemann <dylanthiemann@gmail.com>
2018-05-19 00:09:54 -0700

    working 1st version

--- spoiler-filter.js
+++ spoiler-filter.js
@@ -1,8 +1,9 @@
 var filterSpoilers = function() {
     var episodeCount = document.getElementsByClassName("toggle-episodes");
-    episodeCount.forEach(count => {
-        count.style.display = "none";
-    });
+    console.log("Episode count: ", episodeCount);
+    for (var i = 0; i < episodeCount.length; i ++) {
+        episodeCount.item(i).style.display = "none";
+    }
 }
 
 filterSpoilers();
