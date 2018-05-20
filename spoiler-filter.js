a13d7ee09f91515a9715fc302100132239680319
Dylan Thiemann <dylanthiemann@gmail.com>
2018-05-20 16:39:13 -0700

    include actor page as well

--- spoiler-filter.js
+++ spoiler-filter.js
@@ -1,7 +1,11 @@
 var filterSpoilers = function() {
     var episodeCount = document.getElementsByClassName("toggle-episodes");
-    console.log("Episode count: ", episodeCount);
     for (var i = 0; i < episodeCount.length; i ++) {
+        episodeCount.item(i).style.display = "none";
+    }
+
+    episodeCount = document.getElementsByClassName("filmo-episodes");
+    for (var i = 0; i < episodeCount.length; i++) {
         episodeCount.item(i).style.display = "none";
     }
 }
